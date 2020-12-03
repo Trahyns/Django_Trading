71e7194da7dd5adc47ae770ecc9e1d65976d74ad
Toni R <trahyns@gmail.com>
2020-12-02 21:09:13 -0500

    update urls.py

--- paper_trading/paper_trading/urls.py
+++ paper_trading/paper_trading/urls.py
@@ -13,9 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf.urls import url, include
+from django.shortcuts import redirect
+
+from accounts import views as core_views
 from django.contrib import admin
 from django.urls import path
-
+from trader.views import leaderboard_list_view, index, search, buy, sell, home_view
+from accounts.views import portfolio, signup, logout_user, change_password, account_settings, change_info
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('leaderboard/', leaderboard_list_view),
+    url(r'^signup/$', core_views.signup, name='signup'),
+    path('accounts/', include('django.contrib.auth.urls')),  # new
+    path('accounts/portfolio/', portfolio),
+    path('index/', index),
+    path('search/', search),
+    path('accounts/buy/', buy),
+    path('accounts/sell/', sell),
+    path('accounts/signup/', signup),
+    path('accounts/logout/', logout_user),
+    path('accounts/account_settings/', account_settings),
+    path('accounts/change_password/', change_password),
+    path('accounts/change_info/', change_info),
+    path('home/', home_view),
+    path("", home_view)
 ]
