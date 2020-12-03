abc4b5aca2a3b1c69e90419f6f9a90952d45d57f
Toni R <trahyns@gmail.com>
2020-12-02 19:42:01 -0500

    create django_admin project

---
+++ paper_trading/paper_trading/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for paper_trading project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'paper_trading.settings')
+
+application = get_asgi_application()
